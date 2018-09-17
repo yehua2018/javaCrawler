743c5d5d4bf78eaa154b5b6e2e97ee27fa98ce47
KillersDeath <KillersDeath@users.noreply.github.com>
2018-09-17 15:54:23 +0800

    add java connect mysql

--- src/main/java/com/java/sl/Page.java
+++ src/main/java/com/java/sl/Page.java
@@ -3,7 +3,6 @@ package com.java.sl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Page {
--- src/main/java/com/java/sl/SqlDB.java
+++ src/main/java/com/java/sl/SqlDB.java
@@ -1,20 +1,50 @@
 package com.java.sl;
 
-public class SqlDB {
-    /*
-    构造方法
-     */
-    public SqlDB(String host, int port, String username, String password){
-        //todo
-    }
+import java.sql.*;
 
-//    public getConn(){
-//        //todo
-//    }
-//    /*
-//    执行sql的通用方法
-//     */
-//    public execute(){
-//        //todo
-//    }
+public class SqlDB {
+    static final String DRIVER = "com.mysql.jdbc.Driver";
+    String host = "jdbc:mysql://localhost:3306/studb";
+    String username = "root";
+    String password = "";
+    Connection conn;
+
+
+    public Connection getConn(){
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(host, username, password);
+        }catch(ClassNotFoundException e) {
+            //数据库驱动类异常处理
+            System.out.println("Sorry,can`t find the Driver!");
+            e.printStackTrace();
+        }catch(SQLException e) {
+            //数据库连接失败异常处理
+            e.printStackTrace();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+    /*
+    执行sql的通用方法
+     */
+    public ResultSet execute(String sql){
+        // 获取数据库连接
+        if(conn == null){
+            conn = getConn();
+        }
+        // 执行sql
+        ResultSet resultSet = null;
+        try{
+            Statement statement = conn.createStatement();
+            resultSet = statement.executeQuery(sql);
+        }catch (SQLException e){
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
 }
