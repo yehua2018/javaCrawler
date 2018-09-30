ffb3c5b2ab2e81932d8448ea7d2b6b0eace566f3
HT <you@example.com>
2018-09-30 16:34:00 +0800

    add class Rent

--- src/main/java/com/java/sl/bean/Rent.java
+++ src/main/java/com/java/sl/bean/Rent.java
@@ -6,6 +6,35 @@ package com.java.sl.bean;
  * Email: liujiezhangbupt@gmail.com
  * Description:
  */
-public class Rent {
+public class Rent extends House{
+    public Rent() {
 
+    }
+    private String rentType;//租赁方式
+    private String payTye;//付款方式
+    private String vacantOrNot;//房屋状况
+
+    public String getRentType() {
+        return rentType;
+    }
+
+    public void setRentType(String rentType) {
+        this.rentType = rentType;
+    }
+
+    public String getPayTye() {
+        return payTye;
+    }
+
+    public void setPayTye(String payTye) {
+        this.payTye = payTye;
+    }
+
+    public String getVacantOrNot() {
+        return vacantOrNot;
+    }
+
+    public void setVacantOrNot(String vacantOrNot) {
+        this.vacantOrNot = vacantOrNot;
+    }
 }
